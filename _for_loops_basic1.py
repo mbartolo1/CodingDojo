99001d1e6b1ca2837313148e7db93ad6c6b0fdeb
mbartolo1 <71286059+mbartolo1@users.noreply.github.com>
2020-11-30 15:25:47 -0800

    Add files via upload
    loops practice problems in python.

---
+++ _for_loops_basic1.py
@@ -0,0 +1,35 @@
+# print all integers from 0-150
+for x in range(0,151,1):
+    print(x)
+
+#multiples of five, print all the multiples of 5 from 5-1000
+for x in range(0,1001,5):
+    print(x)
+
+#print integers 1-100, if divisible by 5 print "coding" instead, if divisible by 10 print "coding dojo".
+for x in range(1, 101,1):
+    if x/5 == 0:
+        print("coding")
+    elif x/10 == 0:
+        print("coding dojo")
+
+#add odd integers from 0-5000,000 and print the final sum
+for x in range (0,500000,2):
+    if x%2!==0
+        console.log(sum)
+    else x%2 == 0
+        pass
+sum = sum + [x]
+return(sum)
+
+#print positive numbers starting at 2018, counting down by fours
+for x in range (2018,1,-4):
+    print(x)
+
+#flexible counter
+flexcounter = { "lowNum", "highNum", "Mult"}
+for x in range (1,10,flex(len)):
+    "lowNum" = "Mult"
+    "highNum" = "lowNum" + "Mult"
+    "Mult" = "Mult" * "Mult"
+print(flexcounter)
