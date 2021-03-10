bf1b95eba326662eb6471c75c92429566edc2379
mbartolo1 <71286059+mbartolo1@users.noreply.github.com>
2021-03-10 15:10:53 -0800

    Add files via upload
     Create a Ninja class
     add an attribute: name
     add an attribute: health
     add a attribute: speed - give a default value of 3
     add a attribute: strength - give a default value of 3
     add a method: sayName() - This should log that Ninja's name to the console
     add a method: showStats() - This should show the Ninja's name, strength, speed, and health.
     add a method: drinkSake() - This should add +10 Health to the Ninja

---
+++ Ninja.js
@@ -0,0 +1,25 @@
+
+class Ninja{
+    constructor(name, health){
+        this.name = name;
+        this.health = health;
+        this.speed = 3;
+        this.strength = 3;
+    }
+sayName(){
+    console.log(this.name);
+}
+showStats(){
+    console.log(this.name);
+    console.log(this.strength);
+    console.log(this.health);
+    console.log(this.speed);
+}
+drinkSake(){
+    console.log(this.health += 10);
+}
+}
+const ninja1 = new Ninja("Hyabusa", 10);
+ninja1.sayName();
+ninja1.showStats();
+ninja1.drinkSake();
