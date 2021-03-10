9ae5fc179f0593611f4bc167dd293b62e001b930
mbartolo1 <71286059+mbartolo1@users.noreply.github.com>
2021-03-10 15:23:16 -0800

    Add files via upload
    const superSensei = new Sensei("Master Splinter");
    superSensei.speakWisdom();
    // -> "What one programmer can do in one month, two programmers can do in two months."
    superSensei.showStats();
    // -> "Name: Master Splinter, Health: 210, Speed: 10, Strength: 10"

--- Ninja.js
+++ Ninja.js
@@ -6,20 +6,37 @@ class Ninja{
         this.speed = 3;
         this.strength = 3;
     }
-sayName(){
+    sayName(){
     console.log(this.name);
 }
-showStats(){
+    showStats(){
     console.log(this.name);
     console.log(this.strength);
     console.log(this.health);
     console.log(this.speed);
 }
-drinkSake(){
+    drinkSake(){
     console.log(this.health += 10);
+}
+}
+class Sensei extends Ninja{
+    constructor(name){
+        super(name);
+        this.health = 200;
+        this.speed = 10;
+        this.wisdom = 10;
+    }
+    speakWisdom(){
+    this.drinkSake();
+    console.log("whatever");
 }
 }
 const ninja1 = new Ninja("Hyabusa", 10);
 ninja1.sayName();
 ninja1.showStats();
 ninja1.drinkSake();
+
+const Sensei1 = new Sensei("Master Splinter");
+Sensei1.speakWisdom();
+// -> "What one programmer can do in one month, two programmers can do in two months."
+Sensei1.showStats();
